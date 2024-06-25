e61931b7a1e38320c57c1c41c7cfcbafd793c8d5
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-25 19:44:42 +0300

    Ex 2 was done

--- WpfApp_25.06/MainWindow.xaml.cs
+++ WpfApp_25.06/MainWindow.xaml.cs
@@ -1,14 +1,13 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_25._06
 {
@@ -19,9 +18,15 @@ namespace WpfApp_25._06
         private bool isServerRunning;
         private readonly int port = 11000;
 
+        private readonly int requestLimit = 10;
+        private readonly TimeSpan requestLimitPeriod = TimeSpan.FromHours(1);
+
+        private Dictionary<string, List<DateTime>> clientRequestLog;
+
         public MainWindow()
         {
             InitializeComponent();
+            clientRequestLog = new Dictionary<string, List<DateTime>>();
         }
 
         private void StartServerButton_Click(object sender, RoutedEventArgs e)
@@ -70,11 +75,23 @@ namespace WpfApp_25._06
                 {
                     var result = await udpServer.ReceiveAsync();
                     string request = Encoding.UTF8.GetString(result.Buffer);
-                    Dispatcher.Invoke(() => ServerLogListBox.Items.Add($"Received: {request}"));
+                    string clientEndpoint = result.RemoteEndPoint.ToString();
 
-                    string response = GetRecipes(request);
+                    Dispatcher.Invoke(() => ServerLogListBox.Items.Add($"Received: {request} from {clientEndpoint}"));
+
+                    string response;
+
+                    if (IsRequestAllowed(clientEndpoint))
+                    {
+                        response = GetRecipes(request);
+                        LogClientRequest(clientEndpoint);
+                    }
+                    else
+                    {
+                        response = "Request limit exceeded. Try again later.";
+                    }
+
                     byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-
                     await udpServer.SendAsync(responseBytes, responseBytes.Length, result.RemoteEndPoint);
                     Dispatcher.Invoke(() => ServerLogListBox.Items.Add($"Sent: {response}"));
                 }
@@ -104,6 +121,31 @@ namespace WpfApp_25._06
         private string GetRecipes(string products)
         {
             return $"Recipes for {products}: Recipe1, Recipe2, Recipe3";
+        }
+
+        private bool IsRequestAllowed(string clientEndpoint)
+        {
+            if (!clientRequestLog.ContainsKey(clientEndpoint))
+            {
+                clientRequestLog[clientEndpoint] = new List<DateTime>();
+            }
+
+            var requestTimes = clientRequestLog[clientEndpoint];
+            DateTime now = DateTime.Now;
+
+            requestTimes.RemoveAll(t => (now - t) > requestLimitPeriod);
+
+            return requestTimes.Count < requestLimit;
+        }
+
+        private void LogClientRequest(string clientEndpoint)
+        {
+            if (!clientRequestLog.ContainsKey(clientEndpoint))
+            {
+                clientRequestLog[clientEndpoint] = new List<DateTime>();
+            }
+
+            clientRequestLog[clientEndpoint].Add(DateTime.Now);
         }
 
         private void ProductListTextBox_GotFocus(object sender, RoutedEventArgs e)
