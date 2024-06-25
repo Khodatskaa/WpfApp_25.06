ffbf4c3f56ff49c4015f97cf24b16bb72c342622
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-25 20:18:54 +0300

    Ex 2 was done

--- WpfApp_25.06/MainWindow.xaml.cs
+++ WpfApp_25.06/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Windows;
@@ -7,36 +9,77 @@ namespace WpfApp_25._06
 {
     public partial class MainWindow : Window
     {
-        UdpClient udpClient;
+        private UdpClient udpServer;
+        private Dictionary<string, Queue<DateTime>> requestQueue = new Dictionary<string, Queue<DateTime>>();
+        private const int RequestLimit = 10;
+        private const int HourlyLimitMinutes = 60;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            udpClient = new UdpClient();
         }
 
-        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            try
+            StartUDPServer();
+        }
+
+        private void StartUDPServer()
+        {
+            udpServer = new UdpClient(12345); 
+            txtStatus.Text = "UDP Server with Request Throttling is running...";
+
+            udpServer.BeginReceive(ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] data = udpServer.EndReceive(ar, ref clientEP);
+            string clientRequest = Encoding.ASCII.GetString(data);
+
+            if (CheckRequestRateLimit(clientEP.Address.ToString()))
             {
-                string serverIP = txtServerIP.Text;
-                int serverPort = Convert.ToInt32(txtServerPort.Text);
-
-                string message = txtMessageToSend.Text;
-                byte[] data = Encoding.ASCII.GetBytes(message);
-
-                udpClient.Send(data, data.Length, serverIP, serverPort);
-
-                IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 0);
-                byte[] responseData = udpClient.Receive(ref serverEP);
-                string responseMessage = Encoding.ASCII.GetString(responseData);
-
-                txtReceivedMessage.Text = $"Server Response: {responseMessage}";
+                string responseMessage = $"Server received: {clientRequest}";
+                byte[] responseData = Encoding.ASCII.GetBytes(responseMessage);
+                udpServer.Send(responseData, responseData.Length, clientEP);
+                Console.WriteLine($"Response sent to {clientEP}");
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Console.WriteLine($"Request from {clientEP} exceeded rate limit. Ignoring request.");
+            }
+
+            udpServer.BeginReceive(ReceiveCallback, null);
+        }
+
+        private bool CheckRequestRateLimit(string clientAddress)
+        {
+            CleanUpRequestQueue();
+
+            if (!requestQueue.ContainsKey(clientAddress))
+            {
+                requestQueue[clientAddress] = new Queue<DateTime>();
+            }
+
+            requestQueue[clientAddress].Enqueue(DateTime.Now);
+
+            return requestQueue[clientAddress].Count <= RequestLimit;
+        }
+
+        private void CleanUpRequestQueue()
+        {
+            foreach (var clientAddress in requestQueue.Keys)
+            {
+                while (requestQueue[clientAddress].Count > 0 && (DateTime.Now - requestQueue[clientAddress].Peek()).TotalMinutes > HourlyLimitMinutes)
+                {
+                    requestQueue[clientAddress].Dequeue();
+                }
+
+                if (requestQueue[clientAddress].Count == 0)
+                {
+                    requestQueue.Remove(clientAddress);
+                }
             }
         }
     }
